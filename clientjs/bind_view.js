d5e750dc7d28cc7cadb1e60e5c173c8f2b413153
zilWatch <zilwatch.io@gmail.com>
2021-05-06 22:23:42 +0800

    Refactored theme settings and add test #2

--- clientjs/bind_view.js
+++ clientjs/bind_view.js
@@ -1,6 +1,41 @@
 // bind_view.js
 // No dependencies
 
+function setThemeLightMode() {
+    $("html").removeClass("dark-mode");
+    $("#toggle_theme_icon").removeClass("fa-sun-o");
+    $("#toggle_theme_icon").addClass("fa-moon-o");
+    $("img").each(function() {  
+        let imgSrc = this.src;
+        if (imgSrc.toLowerCase().indexOf("meta.viewblock.io") !== -1) {
+            let darkQueryIndex = imgSrc.indexOf("?t=dark");
+            if (darkQueryIndex !== -1) {
+                this.src = imgSrc.substr(0, darkQueryIndex);
+            }
+        } else if (imgSrc.indexOf("viewblock-dark") !== -1) {
+            this.src = imgSrc.replace("viewblock-dark.png", "viewblock-light.png");
+        }
+       }); 
+}
+
+function setThemeDarkMode() {
+    $("html").addClass("dark-mode");
+    $("#toggle_theme_icon").removeClass("fa-moon-o");
+    $("#toggle_theme_icon").addClass("fa-sun-o");
+    $("img").each(function() {  
+        let imgSrc = this.src;
+        if (imgSrc.toLowerCase().indexOf("meta.viewblock.io") !== -1) {
+            this.src = imgSrc + "?t=dark";
+        } else if (imgSrc.indexOf("viewblock-light.png") !== -1) {
+            this.src = imgSrc.replace("viewblock-light.png", "viewblock-dark.png");
+        }
+       }); 
+}
+
+/**
+ * --------------------------------------------------------------------------------
+ */
+
 function bindViewLoggedInButton(walletAddress) {
     $('#wallet_connect').hide();
     $('#wallet_address').text(walletAddress);
@@ -156,6 +191,9 @@ if (typeof exports !== 'undefined') {
         ZilpayUtils = require('./zilpay_utils.js');
         ZilpayStatus = ZilpayUtils.ZilpayStatus;
     }
+
+    exports.setThemeLightMode = setThemeLightMode;
+    exports.setThemeDarkMode = setThemeDarkMode;
 
     exports.bindViewLoggedInButton = bindViewLoggedInButton;
     exports.bindViewMainContainer = bindViewMainContainer;
--- clientjs/main.js
+++ clientjs/main.js
@@ -5,9 +5,9 @@ window.addEventListener("load", async () => {
     // Get the user's theme preference from local storage, if it's available
     let currentTheme = localStorage.getItem("theme");
     if (currentTheme === "light") {
-        setLightMode();
-    } else if (currentTheme === "dark") {
-        setDarkMode();
+        setThemeLightMode();
+    } else {
+        setThemeDarkMode();
     }
 
     computeZilPriceInUsd(onZilUsdPriceLoaded);
@@ -51,6 +51,20 @@ $("#wallet_refresh").click(function () {
     window.location.reload();
 });
 
+$("#toggle_theme_btn").click(function() {
+    let isCurrentDark = $("html").hasClass("dark-mode");
+    let theme;
+    if (isCurrentDark) {
+        theme = "light";
+        setThemeLightMode();
+    } else {
+        theme = "dark";
+        setThemeDarkMode();
+    }
+    // Finally, let's save the current preference to localStorage to keep using it
+    localStorage.setItem("theme", theme);
+});
+
 function refreshMainContentData(account) {
     // (1) show main screen
     bindViewMainContainer(ZilpayStatus.connected);
@@ -74,49 +88,4 @@ function refreshMainContentData(account) {
 
     // (7) Get ZIL staking balance
     computeZilStakingBalance(account, onZilStakingBalanceLoaded);
-}
-
-$("#toggle_theme_btn").click(function() {
-    let isCurrentDark = $("html").hasClass("dark-mode");
-    let theme;
-    if (isCurrentDark) {
-        theme = "light";
-        setLightMode();
-    } else {
-        theme = "dark";
-        setDarkMode();
-    }
-    // Finally, let's save the current preference to localStorage to keep using it
-    localStorage.setItem("theme", theme);
-});
-
-function setLightMode() {
-    $("html").removeClass("dark-mode");
-    $("#toggle_theme_icon").removeClass("fa-sun-o");
-    $("#toggle_theme_icon").addClass("fa-moon-o");
-    $("img").each(function() {  
-        let imgSrc = this.src;
-        if (imgSrc.toLowerCase().indexOf("meta.viewblock.io") !== -1) {
-            let darkQueryIndex = imgSrc.indexOf("?t=dark");
-            if (darkQueryIndex !== -1) {
-                this.src = imgSrc.substr(0, darkQueryIndex);
-            }
-        } else if (imgSrc.indexOf("dark") !== -1) {
-            this.src = imgSrc.replace("dark", "light");
-        }
-       }); 
-}
-
-function setDarkMode() {
-    $("html").addClass("dark-mode");
-    $("#toggle_theme_icon").removeClass("fa-moon-o");
-    $("#toggle_theme_icon").addClass("fa-sun-o");
-    $("img").each(function() {  
-        let imgSrc = this.src;
-        if (imgSrc.toLowerCase().indexOf("meta.viewblock.io") !== -1) {
-            this.src = imgSrc + "?t=dark";
-        } else if (imgSrc.indexOf("light") !== -1) {
-            this.src = imgSrc.replace("light", "dark");
-        }
-       }); 
 }
