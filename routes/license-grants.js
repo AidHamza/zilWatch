248fc45fa50afd22d62c93e9e2374d307754f237
zilWatch <zilwatch.io@gmail.com>
2021-06-27 14:03:40 +0800

    Add license

--- routes/license-grants.js
+++ routes/license-grants.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET license-grants. */
 router.get('/', function(req, res, next) {
-  res.send('Surpriseee!!');
+  res.send('None.');
 });
 
 module.exports = router;
