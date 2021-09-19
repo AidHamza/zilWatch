a1534fa31f09eff7297a3e7233eec73d74d2d3b6
zilWatchDev <zilwatch.io@gmail.com>
2021-09-20 01:09:36 +0800

    Update API to include latest price

--- routes/api_tokenprice.js
+++ routes/api_tokenprice.js
@@ -1,3 +1,5 @@
+var constants = require('../constants.js');
+
 var ua = require('universal-analytics');
 var ua_key = 'UA-194858212-1';
 var visitor = ua(ua_key);
@@ -59,7 +61,7 @@ router.get('/lowhigh', function (req, res, next) {
     });
 });
 
-/* GET api for token price, directly queried from redis. */
+/* GET api for individual token price, directly queried from redis. */
 router.get('/', function (req, res, next) {
   visitor.pageview("/api/tokenprice" + req.url, "https://zilwatch.io", "Token Price API").send();
 
@@ -69,15 +71,16 @@ router.get('/', function (req, res, next) {
     res.send("Unsupported query parameters!");
     return;
   }
-
   let redis_key = "zilswap_dex_zrc_tokens_price_in_zil_" + queryRange + "_" + tokenSymbol;
   let redis_key_low = "zilswap_dex_zrc_tokens_price_in_zil_" + queryRange + "_low";
   let redis_key_high = "zilswap_dex_zrc_tokens_price_in_zil_" + queryRange + "_high";
+  let redis_key_zilswap_dex_zrc_tokens_price_in_zil = "zilswap_dex_zrc_tokens_price_in_zil";
 
   redisClient.mget(
     [redis_key,
       redis_key_low,
       redis_key_high,
+      redis_key_zilswap_dex_zrc_tokens_price_in_zil,
     ],
     function (err, reply) {
       let data_arr = null;
@@ -100,6 +103,19 @@ router.get('/', function (req, res, next) {
               data_high = curr_data_high[tokenSymbol];
             }
           }
+          if (reply[3]) {
+            zrc_price_in_zil = JSON.parse(reply[3]);
+            // Add the current (latest) price to the back of the historical price
+            // to show most up to date data point
+            if (tokenSymbol in zrc_price_in_zil) {
+              let currentDate = new Date();
+              let currentTimeSeconds = Math.round(currentDate.getTime() / 1000);
+              data_arr.push({
+                'time': currentTimeSeconds,
+                'value': zrc_price_in_zil[tokenSymbol],
+              });
+            }
+          }
         } catch (ex) {
           console.log(ex);
         }
@@ -113,6 +129,75 @@ router.get('/', function (req, res, next) {
         'low': data_low,
         'high': data_high,
         'data': data_arr,
+      };
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(dataObject));
+    });
+});
+
+
+/* GET api for 24h simple all token price, directly queried from redis. */
+router.get('/24h_simple_all_tokens', function (req, res, next) {
+  visitor.pageview("/api/tokenprice" + req.url, "https://zilwatch.io", "Token Price API").send();
+
+  let redis_key_zilswap_dex_zrc_tokens_price_in_zil = "zilswap_dex_zrc_tokens_price_in_zil";
+
+  // First entry is the current (latest) ZRC token price
+  let redis_key_arr = [redis_key_zilswap_dex_zrc_tokens_price_in_zil];
+  let next_idx = 1;
+  let ticker_to_index_map = {}
+
+  for (let ticker in constants.zrcTokenPropertiesListMap) {
+    ticker_to_index_map[ticker] = next_idx;
+    redis_key_arr.push("zilswap_dex_zrc_tokens_price_in_zil_24h_simple_" + ticker);
+    next_idx++;
+  }
+
+  redisClient.mget(
+    redis_key_arr,
+    function (err, reply) {
+      // Final result map
+      let result_ticker_to_data_map = {};
+
+      // Get the first entry, current (latest) zrc price in zil
+      let zrc_price_in_zil = {};
+
+      if (!err && reply) {
+        try {
+          if (reply[0]) {
+            zrc_price_in_zil = JSON.parse(reply[0]);
+          }
+        } catch (ex) {
+          console.log(ex);
+        }
+
+        let currentDate = new Date();
+        let currentTimeSeconds = Math.round(currentDate.getTime() / 1000);
+        for (let ticker in ticker_to_index_map) {
+          try {
+            let curr_idx = ticker_to_index_map[ticker];
+            if (reply[curr_idx]) {
+              data_arr = JSON.parse(reply[curr_idx]);
+            }
+            if (ticker in zrc_price_in_zil) {
+              data_arr.push({
+                'time': currentTimeSeconds,
+                'value': zrc_price_in_zil[ticker],
+              });
+            }
+            result_ticker_to_data_map[ticker] = data_arr;
+          } catch (ex) {
+            console.log(ex);
+          }
+        }
+      }
+      if (result_ticker_to_data_map.length <= 0) {
+        res.send("Data not available!");
+        return;
+      }
+      let dataObject = {
+        'range': '24h_simple',
+        'data': result_ticker_to_data_map,
       };
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify(dataObject));
